b823e26af96e27fc57bb9b3531fc2dbcd65ae6f3
InExSu <InExSu@bk.ru>
2022-11-01 19:35:04 +0300

    handLocal

--- 2022-10-31.c
+++ 2022-10-31.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
-int even_digits_count(int n) {
-  // число четных цифр, используемых в записи натурального числа
+int even_digits_count(int n)
+{ // число четных цифр, используемых в записи натурального числа
   int cnt = 0;
-  for (; n; n /= 10) cnt += (n + 1) & 1;
+  for (; n; n /= 10)
+    cnt += (n + 1) & 1;
   return cnt;
 }
 
-int main(int argc, char *argv[]) {
+void print_array2_int(int *arr, int row_Max, int col_Max)
+{ // массив двумерный на экран
+  for (int row = 0; row < row_Max; row++)
+  {
+    for (int col = 0; col < col_Max; col++)
+      printf("%5d", *(arr + row * col_Max + col));
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[])
+{
   printf("число четных цифр, используемых в записи натурального числа %d\n",
          even_digits_count(11124));
+
+  int m[3][5] = {0};
+  print_array2_int(&m[0][0], 3, 4);
 }
